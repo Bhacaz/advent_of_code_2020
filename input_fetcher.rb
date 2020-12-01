c94f9e3909c066cffbee2fa67ccc40e74f6e58df
Jean-Francis Bastien <bhacaz@gmail.com>
2020-12-01 13:13:38 -0500

    Update input_fetcher.rb to use file cache

--- input_fetcher.rb
+++ input_fetcher.rb
@@ -6,19 +6,33 @@ require 'cgi'
 class InputFetcher
   class << self
     def day(day)
-      uri = URI("https://adventofcode.com/2020/day/#{day}/input")
-      http = Net::HTTP.new(uri.host, 443)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri.request_uri)
-      cookie = CGI::Cookie.new('session', token)
-      request['Cookie'] = cookie.to_s
-      http.request(request).body.split("\n")
+      if file_cache?
+        File.read('input.txt')
+      else
+        download_input(day)
+      end
     end
 
     private
 
     def token
       File.read('/Users/bhacaz/Documents/advent_of_code_2020/token.txt')
+    end
+
+    def file_cache?
+      File.exist?('input.txt')
+    end
+
+    def download_input(day)
+      uri = URI("https://adventofcode.com/2020/day/#{day}/input")
+      http = Net::HTTP.new(uri.host, 443)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      cookie = CGI::Cookie.new('session', token)
+      request['Cookie'] = cookie.to_s
+      content = http.request(request).body
+      File.write('input.txt', content)
+      content
     end
   end
 end
