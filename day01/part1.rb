18fc2b517ef72ff86239cd49e2b39bf7da631f39
Jean-Francis Bastien <bhacaz@gmail.com>
2020-12-01 13:14:05 -0500

    Update day01 to match input_fetcher changes

--- day01/part1.rb
+++ day01/part1.rb
@@ -1,4 +1,4 @@
 require_relative '../input_fetcher'
 
-INPUT = InputFetcher.day(1).map(&:to_i)
-puts INPUT.combination(2).detect { |ints| ints.reduce(&:+) == 2020 }.reduce(&:*) # => 1006176
+INPUT = InputFetcher.day(1).split("\n").map(&:to_i)
+puts INPUT.combination(2).detect { |ints| ints.sum == 2020 }.reduce(:*) # => 1006176
--- day01/part2.rb
+++ day01/part2.rb
@@ -1,4 +1,4 @@
 require_relative '../input_fetcher'
 
-INPUT = InputFetcher.day(1).map(&:to_i)
-puts INPUT.combination(3).detect { |ints| ints.reduce(&:+) == 2020 }.reduce(&:*) # => 199132160
+INPUT = InputFetcher.day(1).split("\n").map(&:to_i)
+puts INPUT.combination(3).detect { |ints| ints.sum == 2020 }.reduce(:*) # => 199132160
